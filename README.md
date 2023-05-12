870b08a37ef0ef4b2ec3b29b9f2616bb2bcd38d0
yxm <yxm.dev@gmx.ie>
2023-05-12 17:38:32 -0300

    ...

---
+++ README.md
@@ -0,0 +1,2 @@
+`web.sh` is a bash function focused on people that like to work inside a terminal emulator but also need  to
+use a browser. It makes easy to search in browsers or open bookmarked pages from terminal commands.
---
+++ web.sh
@@ -0,0 +1,51 @@
+#! /bin/bash 
+
+installdir=~/.config/web.sh
+source $installdir/cfile
+
+    function web(){
+        while [[ "$1" == "-s" ]] || [[ "$1" == "--search" ]]; do
+            if [[ -z $2 ]]; then 
+                read -e -r -p "Search in ${main_search_engine} for: " main_term
+                ${main_search_engine}_function $main_term
+                ${main_browser}_function $search
+                exit
+            fi
+            for i in ${!Ns[@]}; do
+                if [[ "$2" == "${search_engine[$i]}" ]] && [[ -z $3 ]]; then
+                    read -e -r -p "Search in ${search_engine[$i]} for: " term
+                    ${search_engine[$i]}_function $term
+                    ${main_browser}_function $search
+                    exit
+                fi
+            done
+            for i in ${!Ns[@]}; do
+            for j in ${!Nb[@]}; do
+                if [[ "$2" == "${search_engine[$i]}" ]] && 
+                    [[ "$3" == "${browser[$j]}" ]] || 
+                    [[ "$2" == "${browser[$j]}" ]] && 
+                    [[ "$3" == "${search_engine[$i]}" ]]; then
+                        read -e -r -p "Search in ${search_engine[$i]}, from ${browser[$j]}, for: " term
+                        ${search_engine[$i]}_function $term
+                        ${browser_function[$j]} $search
+                        exit
+                fi
+            done    
+            done
+                ${main_search_engine}_function $2 $3 $4 $5 $6
+                ${main_browser}_function $search
+            done
+
+        if [[ "$1" == "-c" ]] || [[ "$1" == "--config" ]]; then
+            echo "executing config..."
+        elif [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+            echo "displaying help..."
+        else
+            for i in ${!Nb[@]}; do
+                if [[ "$main_browser" == "${browser[$i]}" ]]; then
+                   ${browser_function[$i]} $1
+
+                fi
+            done
+        fi
+        }
